9070bf7833b4e8aee70a84c8c033e5607dfda1c5
Lidor Baum <Lidor5500@gmail.com>
2022-03-13 17:16:37 +0200

    added telegram bot

--- backend/models/Visitor.js
+++ backend/models/Visitor.js
@@ -1,5 +1,4 @@
 const db = require("./db-connections/Lostie-db");
-const Libs = require("../libs");
 
 const Schema = db.mongoose.Schema;
 
@@ -16,23 +15,6 @@ const VisitorSchema = Schema(
   }
 );
 
-VisitorSchema.statics.getAllVisitors = function () {
-  return this.find({
-    _id: {
-      $in: productsIds,
-    },
-  });
-};
-
-// ProductSchema.statics.updateProduct = async function (productObj) {
-//   return this.findOneAndUpdate(
-//     { _id: productObj._id },
-//     {
-//       $set: {},
-//     },
-//     { new: true }
-//   );
-// };
 
 VisitorSchema.statics.createVisitor = function (ip) {
   return this.create(ip);
@@ -43,12 +25,9 @@ VisitorSchema.statics.getAllVisitors = function () {
   return this.find({}).sort({_id: -1}).exec();
 };
 
-// ProductSchema.statics.getProductById = function (productId) {
-//   return this.findById(productId);
-// };
+VisitorSchema.statics.isAlertNeeded = function (ip) {
+  return this.find({ip: ip, "createdAt": { $gt: new Date(Date.now() - 60*60 * 1000) } })
+}
 
-// ProductSchema.statics.deleteProduct = function (productId) {
-//   return this.deleteOne({ _id: productId });
-// };
 
 exports.VisitorModel = db.connection.model("Visitor", VisitorSchema);
